e542e8273e57ec3a5cc773035919e3af72e764cd
JuanIgnacioBellavitis <juan.bellavitis@gmail.com>
2023-08-08 18:40:03 -0300

    deployment test

--- components/About.tsx
+++ components/About.tsx
@@ -18,7 +18,7 @@ export default function About() {
             id="about"
             ref={ref}
         >
-            <SectionHeading title="About me" />
+            <SectionHeading title="About me!" />
             <p
                 className='mb-3'
             >
