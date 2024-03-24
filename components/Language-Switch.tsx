305447150e4ec0ce09768ed0ca563b9bfece1db2
JuanIgnacioBellavitis <juan.bellavitis@gmail.com>
2024-03-23 22:13:01 -0300

    Prototype zIndex

--- components/Language-Switch.tsx
+++ components/Language-Switch.tsx
@@ -11,11 +11,11 @@ export default function LanguageSwitch() {
   };
 
   return (
-    <div className="relative">
+    <div className="relative" style={{ zIndex: 9999 }}>
       <select
         className="fixed top-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem]
       border border-gray-950 border-opacity-40 shadow-2x1 rounded-full flex items-center justify-center 
-      hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 dark:border-white custom-select"
+      hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 dark:border-white custom-select z-auto"
         onChange={(e) => changeLanguage(e.target.value)}
       >
         <option value="en">🇪🇳</option>
